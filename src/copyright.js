d337323970617b12151d69a37dc4e3f7f72e9b49
Matt Freels <matt@freels.name>
2012-03-07 19:37:39 -0800

    version, desc

--- src/copyright.js
+++ src/copyright.js
@@ -1,5 +1,5 @@
 /*!
-  * Promise.js: Monadic, composable async futures
+  * Promise.js: Flexible, composable, monadic futures for javascript
   * (c) Matt Freels 2012
   * https://github.com/freels/promise.js
   * license MIT
